fe8a3cae1738b69dac97bb4c761f6433f3f7d562
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-05-28 20:34:54 -0500

    Made Navbar more mobile responsive

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,13 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import styled from 'styled-components';
 
-import { useWindowSize } from '../../hooks/useWindowSize';
-
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
-
-    const size = useWindowSize();
-    console.log(size)
 
     const navItems = [
         { path: '/', label: 'Home' },
@@ -17,16 +12,19 @@ const Navbar = () => {
         { path: '/login', label: 'Login' }
     ];
 
+    const handleToggleMenu = () => {
+
+        setToggleMenu(!toggleMenu)
+    }
 
     return (
-        // <Nav className='d-flex flex-wrap w-100 justify-content-end'>
         <div className="container-fluid shadow-sm">
             <Nav className="row">
                 <div className='col-9 d-flex flex-wrap align-items-center justify-content-center'>
                     {toggleMenu && <>
                         {navItems.map((item, idx) => (
                             <NavLink key={`navlink-item-${item.label}-#${idx}`}
-                                onClick={() => setToggleMenu(false)}
+                                onClick={handleToggleMenu}
                                 style={{ "backgroundColor": "#4f6a6a", "color": "#dadfdf" }}
                                 className="btn col-xs-12 col-sm-9 col-md-6 col-lg-3 btn-outline-success m-2 shadow-sm"
                                 activeStyle={{ "backgroundColor": "#708685", "color": "#dadfdf" }}
@@ -38,9 +36,9 @@ const Navbar = () => {
                     </>}
                 </div>
                 <button
-                    style={{ "fontWeight": "bold", "fontSize": "2rem" }}
-                    onClick={() => setToggleMenu(!toggleMenu)} className="col-2 btn btn-outline-success shadow-lg m-2">
-                    <GiHamburgerMenu />
+                    style={{ "fontWeight": "bold", "fontSize": `${toggleMenu ? '2rem' : '1rem'}` }}
+                    onClick={() => setToggleMenu(!toggleMenu)} className="col-2 d-flex justify-content-center align-center btn btn-outline-success shadow-lg m-2">
+                    <GiHamburgerMenu className='col-lg-6' />
                 </button>
             </Nav>
         </div>
